72f79edf637ad0679b2b47b97a87960023d8e67f
AaratBhaskar <soniasatsangi16@gmail.com>
2024-11-23 23:05:09 +0530

    Add files via upload

---
+++ shopping cart.py
@@ -0,0 +1,15 @@
+cart={}
+print("This is the program you want to add item in your cart and this make a dictionary")
+a=(input(f"Enter you want to add in your cart:"))
+b=(float(input(f"Enter price you want to add in your {a}")))
+cart[a]=b
+a=(input(f"Enter you want to add in your cart:"))
+b=(float(input(f"Enter price you want to add in your {a}")))
+cart[a]=b
+a=(input(f"Enter you want to add in your cart:"))
+b=(float(input(f"Enter price you want to add in your {a}")))
+cart[a]=b
+a=(input(f"Enter you want to add in your cart:"))
+b=(float(input(f"Enter price you want to add in your {a}")))
+cart[a]=b
+print(f"Your cart is ready successfuly :{cart}")
